e8a1fba4d0f077f34c10fe7e097cb2bb23acf7ba
Philippine CLUNIAT <philippine.cluniat@gmail.com>
2019-02-17 17:00:49 +0100

    refactoring font awesome + add common stylesheet

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,26 +1,22 @@
 import React, { Component } from 'react';
 import './Home.scss';
 import Header from "../../Components/Header/Header";
+import FormationContent from "../../Components/FormationContent/FormationContent";
 import Tab from "../../Components/Tab/Tab";
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
-import { faPhone } from '@fortawesome/free-solid-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faMapMarkerAlt, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 class Home extends Component {
 
   render() {
-    library.add(faMapMarkerAlt);
-    library.add(faPhone);
-    library.add(faEnvelope);
-    library.add(faGithub);
-    library.add(faLinkedin);
+    library.add(faMapMarkerAlt,faPhone, faEnvelope, faGithub, faLinkedin);
     return (
         <div>
           <Header/>
-          <Tab title={"Formation"} />
+          <Tab title={"Formation"}>
+            <FormationContent/>
+          </Tab>
           <Tab title={"Projets"} />
           <Tab title={"Compétences"} />
           <Tab title={"Expériences professionnelles"} />
