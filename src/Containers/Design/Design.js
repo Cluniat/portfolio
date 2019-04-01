d38b17bd7154962cc10d9e91cef1224a2d71c04e
Philippine CLUNIAT <philippine.cluniat@gmail.com>
2019-04-01 14:29:08 +0200

    add design container

---
+++ src/Containers/Design/Design.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+
+class Design extends Component {
+
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+
+        );
+    }
+}
+
+
+export default Design;
