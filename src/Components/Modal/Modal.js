44e893a430661a311f097ec8b680634da76f85fd
Philippine CLUNIAT <pcluniat@pentalog.com>
2020-12-12 10:00:36 +0100

    change modal class component to fucntion component

--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,58 +1,55 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Modal.scss';
 import PropTypes from 'prop-types';
 
 
 
-class Modal extends Component {
+const Modal = ({visible, onClose, title, children}) => {
+    const [animateIn, setAnimateIn] = useState(!visible);
+    const [containerClass, setContainerClass] = useState(["one"]);
+    const [modalClass, setModalClass] = useState(["modal-component"]);
 
-    constructor(props) {
-        super(props);
-        this.state = {animateIn: !this.props.visible};
-    }
-
-    toggle() {
-        this.setState({animateIn: !this.state.animateIn});
-    }
-    close() {
-        this.toggle();
-        setTimeout(() => this.props.onClose(), 1000);
-    }
-
-    render() {
-        let containerClass= ["one"];
-        let modalClass = ["modal-component"];
-
-        if(this.props.visible) {
-            if(this.state.animateIn) {
-                containerClass.push("out");
+    useEffect(() => {
+        if(visible) {
+            if(animateIn) {
+                setContainerClass([...containerClass, "out"])
             } else {
-                modalClass.push("modal-active");
+                setModalClass([...modalClass, "modal-active"])
             }
         }
-        return (
-            <div className={modalClass.join(' ')}>
-                <div id="modal-container" className={containerClass.join(' ')} onClick={this.close.bind(this)}>
-                    <div className="modal-background">
-                        <div className="modal">
-                            <div className={"modal-content"}>
-                                <div className={"title"}>
-                                    {this.props.title.toUpperCase()}
-                                </div>
-                                <div className={"tab-content"}>
-                                    {this.props.children}
-                                </div>
+    }, [visible, animateIn])
+
+    const toggle = () => {
+        setAnimateIn(!animateIn);
+    }
+
+    const close = () => {
+        toggle();
+        setTimeout(() => onClose(), 1000);
+    }
+
+    return (
+        <div className={modalClass.join(' ')}>
+            <div id="modal-container" className={containerClass.join(' ')} onClick={close}>
+                <div className="modal-background">
+                    <div className="modal">
+                        <div className={"modal-content"}>
+                            <div className={"title"}>
+                                {title.toUpperCase()}
                             </div>
-                            <svg className="modal-svg" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%"
-                                 preserveAspectRatio="none">
-                                <rect x="0" y="0" fill="none" width="226" height="162" rx="3" ry="3"/>
-                            </svg>
+                            <div className={"tab-content"}>
+                                {children}
+                            </div>
                         </div>
+                        <svg className="modal-svg" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%"
+                             preserveAspectRatio="none">
+                            <rect x="0" y="0" fill="none" width="226" height="162" rx="3" ry="3"/>
+                        </svg>
                     </div>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 Modal.propTypes = {
