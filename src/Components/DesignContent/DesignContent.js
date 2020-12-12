1d5865e6a35927b16c71b2fc9e5f3a6ad73b346f
Philippine CLUNIAT <pcluniat@pentalog.com>
2020-12-12 09:29:43 +0100

    change design content class component to fucntion component

--- src/Components/DesignContent/DesignContent.js
+++ src/Components/DesignContent/DesignContent.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './DesignContent.scss';
 import polytech_2018 from "../../Assets/designs/logo-polytech.jpg";
 import mimi from "../../Assets/designs/logo-mimi.png";
@@ -8,23 +8,16 @@ import logo_bal from "../../Assets/designs/logo-bal.png";
 import hogwec from "../../Assets/designs/logo-hogwec.png";
 
 
-class DesignContent extends Component {
+const DesignContent = () => (
+    <div className={"design-content"}>
+        <img className={"design"} src={polytech_2018} alt={"polytech_2018"}/>
+        <img className={"design"} src={mimi} alt={"mimi"}/>
+        <img className={"design"} src={irhumman} alt={"irhumman"}/>
+        <img className={"design"} src={logo_gone} alt={"logo_gone"}/>
+        <img className={"design"} src={logo_bal} alt={"logo_bal"}/>
+        <img className={"design"} src={hogwec} alt={"hogwec"}/>
 
-    render() {
-        return (
-            <div className={"design-content"}>
-                <img className={"design"} src={polytech_2018} alt={"polytech_2018"}/>
-                <img className={"design"} src={mimi} alt={"mimi"}/>
-                <img className={"design"} src={irhumman} alt={"irhumman"}/>
-                <img className={"design"} src={logo_gone} alt={"logo_gone"}/>
-                <img className={"design"} src={logo_bal} alt={"logo_bal"}/>
-                <img className={"design"} src={hogwec} alt={"hogwec"}/>
-
-            </div>
-        );
-
-    }
-}
-
+    </div>
+)
 
 export default DesignContent;
