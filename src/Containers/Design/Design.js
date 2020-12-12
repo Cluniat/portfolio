172df691f404509e6b571c5b21486377cee4b387
Philippine CLUNIAT <pcluniat@pentalog.com>
2020-12-12 08:49:21 +0100

    change design class component to fucntion component

--- src/Containers/Design/Design.js
+++ src/Containers/Design/Design.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import DetailsHeader from "../../Components/DetailsHeader/DetailsHeader";
 import DetailsBody from "../../Components/DetailsBody/DetailsBody";
 import "./Design.scss"
-import * as ReactDOM from "react-dom";
 
 
-class Design extends Component {
+const Design = () => {
+    const [isLogo, setIsLogo] = useState(true);
+    const [logoBtnClass, setLogoBtnClass] = useState(["design-btn"]);
+    const [posterBtnClass, setPosterBtnClass] = useState(["design-btn"]);
 
-    constructor(props) {
-        super(props);
-        this.state = {isLogo: true};
-    }
+    useEffect(() => {
+        setLogoBtnClass(isLogo ? [...logoBtnClass, "clicked"] : ["design-btn"])
+        setPosterBtnClass(!isLogo ? [...posterBtnClass, "clicked"] : ["design-btn"])
+    }, [isLogo])
 
-    renderDetailsBody(){
-        if (this.state.isLogo) {
+
+    const renderDetailsBody = () => {
+        if (isLogo) {
             return  <DetailsBody isLogo={true}/>
         }
         else {
@@ -21,42 +24,20 @@ class Design extends Component {
         }
     }
 
-    componentDidMount() {
-        ReactDOM.findDOMNode(this.refs.logo).focus();
-    }
-
-
-    render() {
-
-        let logoBtnClass = ["design-btn"];
-        let posterBtnClass = ["design-btn"];
-        if(this.state.isLogo){
-            logoBtnClass.push("clicked");
-            posterBtnClass = ["design-btn"];
-        }
-        else {
-            posterBtnClass.push("clicked");
-            logoBtnClass = ["design-btn"];
-        }
-
-            return (
-            <div className={"design-page"}>
-                <DetailsHeader title={"design"}/>
+    return (
+        <div className={"design-page"}>
+            <DetailsHeader title={"design"}/>
                 <div className={"btns"}>
-                    <button className={logoBtnClass.join(' ')} ref={"logo"} onClick={() => this.setState({isLogo: true})}>
+                    <button className={logoBtnClass.join(' ')} onClick={() => setIsLogo(true)}>
                         Logos
                     </button>
-                    <button className={posterBtnClass.join(' ')} onClick={() => this.setState({isLogo: false})}>
+                    <button className={posterBtnClass.join(' ')} onClick={() => setIsLogo(false)}>
                         Affiches
                     </button>
                 </div>
-                {this.renderDetailsBody()}
+            {renderDetailsBody()}
             </div>
-
-        );
-    }
+    )
 }
-
-
 
 export default Design;
