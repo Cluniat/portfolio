0ffd4abfcbff506b26c2ff94ef18dc3d776f018a
Philippine CLUNIAT <pcluniat@pentalog.com>
2020-12-12 08:35:25 +0100

    add redux hooks to home component

--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useCallback, useEffect} from 'react';
 import './Home.scss';
 import Header from "../../Components/Header/Header";
 import FormationContent from "../../Components/FormationContent/FormationContent";
@@ -9,58 +9,62 @@ import {fab} from '@fortawesome/free-brands-svg-icons';
 import {fas} from '@fortawesome/free-solid-svg-icons';
 import CompetenceContent from "../../Components/CompetenceContent/CompetenceContent";
 import ExperienceContent from "../../Components/ExperienceContent/ExperienceContent";
-import {Link, withRouter} from "react-router-dom";
+import {Link} from "react-router-dom";
 import DesignContent from "../../Components/DesignContent/DesignContent";
 
-import EducationActions from '../../Store/Education/actions';
-import ExperienceActions from '../../Store/Experience/actions';
-import ProjectActions from '../../Store/Project/actions';
-import {connect} from "react-redux";
+import {EducationTypes} from '../../Store/Education/actions';
+import {ExperienceTypes} from '../../Store/Experience/actions';
+import {ProjectTypes} from '../../Store/Project/actions';
+import {useDispatch} from "react-redux";
 
-class Home extends Component {
 
-    componentDidMount() {
-        this.props.getLastEducation()
-        this.props.getLastExperience()
-        this.props.getLastProject()
-    }
+const Home = () => {
+    library.add(fas, fab);
+    const dispatch = useDispatch();
+    const getLastEducation = useCallback(
+        () => dispatch({type: EducationTypes.LAST_EDUCATION}),
+        [dispatch]
+    )
+    const getLastExperience = useCallback(
+        () => dispatch({type: ExperienceTypes.LAST_EXPERIENCE}),
+        [dispatch]
+    )
+    const getLastProject = useCallback(
+        () => dispatch({type: ProjectTypes.LAST_PROJECT}),
+        [dispatch]
+    )
 
-    render() {
-        library.add(fas, fab);
-        return (
-            <div>
-                <Header/>
-                <Tab title={"Formation"} isFormation={true}>
-                    <FormationContent/>
+    useEffect(() => {
+        getLastEducation();
+        getLastExperience();
+        getLastProject();
+    }, [getLastEducation, getLastExperience, getLastProject])
+
+
+    return(
+        <div>
+            <Header/>
+            <Link to={"/project"}>
+                <Tab title={"Projets"} hasModal={false}>
+                    <ProjetContent/>
                 </Tab>
-                <Link to={"/project"}>
-                    <Tab title={"Projets"} hasModal={false}>
-                        <ProjetContent/>
-                    </Tab>
-                </Link>
-                <Tab title={"Compétences"} isCompetence={true}>
-                    <CompetenceContent/>
+            </Link>
+            <Tab title={"Compétences"} isCompetence={true}>
+                <CompetenceContent/>
+            </Tab>
+            <Tab title={"Expériences"} isExperience={true}>
+                <ExperienceContent/>
+            </Tab>
+            <Tab title={"Formation"} isFormation={true}>
+                <FormationContent/>
+            </Tab>
+            <Link to={"/design"}>
+                <Tab title={"Designs"} hasModal={false}>
+                    <DesignContent/>
                 </Tab>
-                <Tab title={"Expériences"} isExperience={true}>
-                    <ExperienceContent/>
-                </Tab>
-                <Link to={"/design"}>
-                    <Tab title={"Designs"} hasModal={false}>
-                        <DesignContent/>
-                    </Tab>
-                </Link>
-            </div>
-        );
-    }
+            </Link>
+        </div>
+    )
 }
 
-const mapDispatchToProps = dispatch => ({
-    getLastEducation: () => dispatch(EducationActions.lastEducation()),
-    getLastExperience: () => dispatch(ExperienceActions.lastExperience()),
-    getLastProject: () => dispatch(ProjectActions.lastProject()),
-})
-
-export default withRouter(connect(
-    null,
-    mapDispatchToProps
-)(Home));
+export default Home;
