0fee68510e63caf6bba35290f4352f669dad10d9
Philippine CLUNIAT <pcluniat@pentalog.com>
2020-12-12 09:28:44 +0100

    change competence content class component to fucntion component

--- src/Components/CompetenceContent/CompetenceContent.js
+++ src/Components/CompetenceContent/CompetenceContent.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './CompetenceContent.scss';
 import vuejs from "../../Assets/images/vuejs.png";
 import reactjs from "../../Assets/images/reactjs.png";
@@ -7,22 +7,15 @@ import java from "../../Assets/images/java.png";
 import laravel from "../../Assets/images/laravel.png";
 import netcore from "../../Assets/images/aspnetcore.png";
 
-class CompetenceContent extends Component {
-
-    render() {
-        return (
-            <div className={"logos"}>
-                <img className={"icon"} src={vuejs} alt={"vuejs"} height={"103px"} width={"103px"}/>
-                <img className={"icon"} src={reactjs} alt={"reactjs"} height={"118px"} width={"114px"}/>
-                <img className={"icon"} src={angular} alt={"angular"} height={"148px"} width={"148px"}/>
-                <img className={"icon"} src={java} alt={"java"} height={"148px"} width={"74px"}/>
-                <img className={"icon"} src={laravel} alt={"laravel"} height={"76px"} width={"222px"}/>
-                <img className={"icon"} src={netcore} alt={"netcore"} height={"112px"} width={"148px"}/>
-            </div>
-        );
-
-    }
-}
-
+const CompetenceContent = () =>  (
+    <div className={"logos"}>
+        <img className={"icon"} src={vuejs} alt={"vuejs"} height={"103px"} width={"103px"}/>
+        <img className={"icon"} src={reactjs} alt={"reactjs"} height={"118px"} width={"114px"}/>
+        <img className={"icon"} src={angular} alt={"angular"} height={"148px"} width={"148px"}/>
+        <img className={"icon"} src={java} alt={"java"} height={"148px"} width={"74px"}/>
+        <img className={"icon"} src={laravel} alt={"laravel"} height={"76px"} width={"222px"}/>
+        <img className={"icon"} src={netcore} alt={"netcore"} height={"112px"} width={"148px"}/>
+    </div>
+)
 
 export default CompetenceContent;
